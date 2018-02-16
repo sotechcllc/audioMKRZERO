268f54891d9384210e6e501dbde50ba77c14dc21
Fluvio Lobo <fluvio.lobo@gmail.com>
2018-02-15 21:51:29 -0500

    Renamed several audio files and tweaked the example script to play brown and white noise on a loop

--- software/teensy/WavFilePlayer/WavFilePlayer.ino
+++ software/teensy/WavFilePlayer/WavFilePlayer.ino
@@ -1,29 +1,10 @@
-// Simple WAV file player example
-//
-// Three types of output may be used, by configuring the code below.
-//
-//   1: Digital I2S - Normally used with the audio shield:
-//         http://www.pjrc.com/store/teensy3_audio.html
-//
-//   2: Digital S/PDIF - Connect pin 22 to a S/PDIF transmitter
-//         https://www.oshpark.com/shared_projects/KcDBKHta
-//
-//   3: Analog DAC - Connect the DAC pin to an amplified speaker
-//         http://www.pjrc.com/teensy/gui/?info=AudioOutputAnalog
-//
-// To configure the output type, first uncomment one of the three
-// output objects.  If not using the audio shield, comment out
-// the sgtl5000_1 lines in setup(), so it does not wait forever
-// trying to configure the SGTL5000 codec chip.
-//
-// The SD card may connect to different pins, depending on the
-// hardware you are using.  Uncomment or configure the SD card
-// pins to match your hardware.
-//
-// Data files to put on your SD card can be downloaded here:
-//   http://www.pjrc.com/teensy/td_libs_AudioDataFiles.html
-//
-// This example code is in the public domain.
+/*
+ * Wave File Playback
+ * 
+ * Script design to play audio files stored in SD card
+ * 
+ * 02152018
+ */
 
 #include <Audio.h>
 #include <Wire.h>
@@ -45,15 +26,12 @@ AudioControlSGTL5000     sgtl5000_1;
 #define SDCARD_MOSI_PIN  7
 #define SDCARD_SCK_PIN   14
 
-// Use these with the Teensy 3.5 & 3.6 SD card
-//#define SDCARD_CS_PIN    BUILTIN_SDCARD
-//#define SDCARD_MOSI_PIN  11  // not actually used
-//#define SDCARD_SCK_PIN   13  // not actually used
-
-// Use these for the SD+Wiz820 or other adaptors
-//#define SDCARD_CS_PIN    4
-//#define SDCARD_MOSI_PIN  11
-//#define SDCARD_SCK_PIN   13
+// Variables
+//const char    filename1[] = "1hz0p8.WAV";
+//const char    filename2[] = "10hz0p8.WAV";
+//const char    filename3[] = "100hz0p8.WAV";
+const char    filename4[] = "BNOISE.WAV";
+const char    filename5[] = "WNOISE.WAV";
 
 void setup() {
   Serial.begin(9600);
@@ -103,13 +81,15 @@ void playFile(const char *filename)
 
 
 void loop() {
-  playFile("SDTEST1.WAV");  // filenames are always uppercase 8.3 format
+  //playFile(filename1);  // filenames are always uppercase 8.3 format
+  //delay(500);
+  //playFile(filename2);
+  //delay(500);
+  //playFile(filename3);
+  //delay(500);
+  playFile(filename4);
   delay(500);
-  playFile("SDTEST2.WAV");
+  playFile(filename5);
   delay(500);
-  playFile("SDTEST3.WAV");
-  delay(500);
-  playFile("SDTEST4.WAV");
-  delay(1500);
 }
 
